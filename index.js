a8164a913b275713e2afc46ca4dcee4fbd25d6d7
GabSalinas110403 <gab.salinas15@gmail.com>
2024-10-25 21:35:41 -0600

    acambio

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-var express = require("express");
-var rutas = require("./routes/citasRutas");
-
-var app = express();
-
+var express=require("express");
+var rutas=require("./routes/citasRutas");
+ 
+var app=express();
 app.set("view engine", "ejs");
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
+app.use("/",rutas);
 
-app.use("/", rutas);
 
-var port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log("Servidor en http://localhost:" + port);
+var port=process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log("Servidor en http://localhost:"+port);
 });
